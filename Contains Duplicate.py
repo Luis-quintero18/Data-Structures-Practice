edbe0003bdfe88f720f4fc4cf9d034677e5c9443
Luis Quintero <lquintero1899@gmail.com>
2025-08-16 16:46:20 -0400

    Completed Contains Duplicate Algo

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,6 +1,22 @@
 """
 Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+Input: nums = [1, 2, 3, 3]
+
+Output: true
 """
 
 def contains_duplicates(nums):
-    pass
+    seen = {}
+
+    for num in nums:
+        if num in seen:
+            return True
+        else:
+            seen[num] = 1
+    return False
+
+true_list = [1, 2, 3, 3]
+false_list = [1, 2, 3, 4]
+
+print(contains_duplicates(true_list))
+print(contains_duplicates(false_list))
